4ffdb4c1e6a53a2e00fa55117a6ecb2a4f18bc53
jonas-martinez-Desktop <cbobbxta.fr@gmail.com>
2020-12-07 01:18:19 +0100

    Suppression de code inutile

--- www/js/script.js
+++ www/js/script.js
@@ -1,9 +1,3 @@
-loadfiles_button = document.getElementById("br_loadfiles");
-loadfiles_button.onclick = function () {
-    console.log('clicked');
-};
-console.log(loadfiles_button);
-
 const fileSelector = document.getElementById('file-selector');
 fileSelector.addEventListener('change', (event) => {
     const fileList = event.target.files;
