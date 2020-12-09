485ca441dfa71d97360beee35ebba69176b194d9
mantelme <mathis.antelme@gmail.com>
2020-12-09 18:13:17 +0100

    :building_construction: - Changement architectural, Utilisation d'un serveur NodeJS

---
+++ app/index.js
@@ -0,0 +1,23 @@
+var http = require('http')
+var fs = require('fs')
+
+const address = '127.0.0.1';
+const port = 5050;
+
+fs.readFile('./app/html/index.html', function(err, html) {
+    if (err) {
+        throw err;
+    }
+
+    http.createServer(function(req, res) {
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(html);
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write('Not Found')
+        }
+
+        res.end();
+    }).listen(port, address);
+});
